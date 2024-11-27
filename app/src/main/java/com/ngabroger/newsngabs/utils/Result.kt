7836073b4342661027f4a96692b7e2b3adc34b46
rogers <arbination13@gmail.com>
2024-11-27 12:49:10 +0700

    feat: update structure

--- app/src/main/java/com/ngabroger/newsngabs/utils/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.ngabroger.newsngabs.utils
-
-sealed class Result<out R> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: String) : Result<Nothing>()
-    object Loading : Result<Nothing>()
-}
